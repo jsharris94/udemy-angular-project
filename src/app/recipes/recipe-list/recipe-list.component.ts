923a5a0385cbaa62b0f3b5093640d89436332293
Joseph Harris <joeharris1017@gmail.com>
2017-12-09 11:09:10 -0800

    worked on recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Burger-Food-Fries-Menu-Cheeseburger-Drink-Meal-34314.png'),
+    new Recipe('A Test Recipe', 'This is a test', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Burger-Food-Fries-Menu-Cheeseburger-Drink-Meal-34314.png'),
+  ];
 
   constructor() { }
 
